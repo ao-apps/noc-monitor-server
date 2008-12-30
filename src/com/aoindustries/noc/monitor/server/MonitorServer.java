d31570952c92138673836248da697cf65a9b0060
Dan Armstrong <dan@aoindustries.com>
2008-12-30 18:01:06 +0000

    Initial import

---
+++ src/com/aoindustries/noc/monitor/server/MonitorServer.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2008 by AO Industries, Inc.,
+ * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
+ * All rights reserved.
+ */
+package com.aoindustries.noc.monitor.server;
+
+import com.aoindustries.aoserv.client.AOServClientConfiguration;
+import com.aoindustries.noc.common.Monitor;
+import com.aoindustries.rmi.RMIClientSocketFactorySSL;
+import com.aoindustries.rmi.RMIClientSocketFactoryTCP;
+import com.aoindustries.rmi.RMIServerSocketFactorySSL;
+import com.aoindustries.rmi.RMIServerSocketFactoryTCP;
+import com.aoindustries.noc.monitor.MonitorImpl;
+import com.aoindustries.util.ErrorHandler;
+import com.aoindustries.util.ErrorPrinter;
+import com.aoindustries.util.StandardErrorHandler;
+import java.io.File;
+import java.net.InetAddress;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.util.Locale;
+
+/**
+ * The RMI server for NOC monitoring.  If a username and password are available in the aoserv-client.properties file,
+ * will auto-login at startup to kick-start the monitoring.
+ *
+ * @author  AO Industries, Inc.
+ */
+public class MonitorServer {
+
+    final static ErrorHandler errorHandler = new StandardErrorHandler();
+
+    public static void main(String[] args) {
+        int port = Monitor.DEFAULT_RMI_SERVER_PORT;
+        String listenAddress = null;
+        String publicAddress = null;
+        if(args.length>=1) port = Integer.parseInt(args[0].trim());
+        if(args.length>=2) listenAddress = args[1].trim();
+        if(args.length>=3) publicAddress = args[2].trim();
+        if(args.length>3) {
+            System.err.println("usage: "+MonitorServer.class.getName()+" [port [listen_address [public_address]]]");
+            System.err.println("\tport            the server port - defaults to "+Monitor.DEFAULT_RMI_SERVER_PORT);
+            System.err.println("\tlisten_address  the local address the server will bind to - the private IP/hostname if behind NAT");
+            System.err.println("\tpublic_address  the public address that will reach the server - the external IP/hostname if behind NAT");
+            System.exit(1);
+            return;
+        }
+        if(System.getSecurityManager()==null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            // Setup the RMI system properties
+            if(publicAddress!=null && publicAddress.length()>0) {
+                System.setProperty("java.rmi.server.hostname", publicAddress);
+            } else if(listenAddress!=null && listenAddress.length()>0) {
+                System.setProperty("java.rmi.server.hostname", listenAddress);
+            } else {
+                System.clearProperty("java.rmi.server.hostname");
+            }
+            System.setProperty("java.rmi.server.randomIDs", "true");
+            System.setProperty("java.rmi.server.useCodebaseOnly", "true");
+            System.setProperty("java.rmi.server.disableHttp", "true");
+
+            RMIClientSocketFactory csf;
+            RMIServerSocketFactory ssf;
+            if(
+                listenAddress!=null
+                && (
+                    listenAddress.equalsIgnoreCase("localhost")
+                    || listenAddress.equalsIgnoreCase("localhost.localdomain")
+                    || listenAddress.equals("127.0.0.1")
+                    || InetAddress.getByName(listenAddress).isLoopbackAddress()
+                )
+            ) {
+                // Non-SSL for anything loopback
+                csf = new RMIClientSocketFactoryTCP();
+                ssf = new RMIServerSocketFactoryTCP(listenAddress);
+            } else {
+                if(System.getProperty("javax.net.ssl.keyStorePassword")==null) {
+                    System.setProperty(
+                        "javax.net.ssl.keyStorePassword",
+                        "changeit"
+                    );
+                }
+                if(System.getProperty("javax.net.ssl.keyStore")==null) {
+                    System.setProperty(
+                        "javax.net.ssl.keyStore",
+                        System.getProperty("user.home")+File.separatorChar+".keystore"
+                    );
+                }
+
+                // SSL for anything else
+                if(listenAddress!=null && listenAddress.length()>0) {
+                    csf = new RMIClientSocketFactorySSL();
+                    ssf = new RMIServerSocketFactorySSL(listenAddress);
+                } else {
+                    csf = new RMIClientSocketFactorySSL();
+                    ssf = new RMIServerSocketFactorySSL();
+                }
+            }
+            Registry registry = LocateRegistry.createRegistry(port, csf, ssf); //LocateRegistry.getRegistry();
+            Monitor monitor = new MonitorImpl(errorHandler, port, csf, ssf);
+            registry.rebind("com.aoindustries.noc.monitor.server.MonitorServer", monitor);
+
+            // Auto-login with a top-level account to kick-off the monitoring if a username/password exist in the aoserv-client.properties file
+            String rootUsername = AOServClientConfiguration.getUsername();
+            String rootPassword = AOServClientConfiguration.getPassword();
+            if(
+                rootUsername!=null && (rootUsername=rootUsername.trim()).length()>0
+                && rootPassword!=null && (rootPassword=rootPassword.trim()).length()>0
+            ) {
+                int attemptsLeft = 120;
+                while(attemptsLeft>0) {
+                    try {
+                        monitor.login(Locale.getDefault(), rootUsername, rootPassword);
+                        break;
+                    } catch(Exception err) {
+                        errorHandler.reportError(err, null);
+                        try {
+                            Thread.sleep(60000);
+                        } catch(InterruptedException err2) {
+                            errorHandler.reportWarning(err2, null);
+                        }
+                    }
+                    attemptsLeft--;
+                }
+            }
+        } catch(Exception err) {
+            ErrorPrinter.printStackTraces(err);
+        }
+    }
+
+    private MonitorServer() {
+    }
+}
