db7d70922fd34d500bb63774350dec1ebf9c9115
Dan Armstrong <dan@aoindustries.com>
2013-05-29 05:17:00 +0000

    Validator types

--- src/com/aoindustries/noc/monitor/server/MonitorServer.java
+++ src/com/aoindustries/noc/monitor/server/MonitorServer.java
@@ -1,17 +1,23 @@
 /*
- * Copyright 2008-2012 by AO Industries, Inc.,
+ * Copyright 2008-2009 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
 package com.aoindustries.noc.monitor.server;
 
 import com.aoindustries.aoserv.client.AOServClientConfiguration;
-import com.aoindustries.noc.monitor.common.Monitor;
-import com.aoindustries.noc.monitor.common.MonitoringPoint;
+import com.aoindustries.rmi.RMIClientSocketFactorySSL;
+import com.aoindustries.rmi.RMIClientSocketFactoryTCP;
+import com.aoindustries.rmi.RMIServerSocketFactorySSL;
+import com.aoindustries.rmi.RMIServerSocketFactoryTCP;
 import com.aoindustries.noc.monitor.MonitorImpl;
-import com.aoindustries.noc.monitor.mobile.server.MobileServer;
-import com.aoindustries.noc.monitor.rmi.server.RmiServerMonitor;
+import com.aoindustries.noc.monitor.common.Monitor;
+import java.io.File;
 import java.net.InetAddress;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,33 +32,18 @@ public class MonitorServer {
 
     private static final Logger logger = Logger.getLogger(MonitorServer.class.getName());
 
-    /**
-     * Keeps a reference to factory to avoid complete garbage collection.
-     * @{link http://stackoverflow.com/questions/645208/java-rmi-nosuchobjectexception-no-such-object-in-table}
-     */
-    private static RmiServerMonitor monitorServer;
-
-    private static MobileServer mobileServer;
-
     public static void main(String[] args) {
         int port = Monitor.DEFAULT_RMI_SERVER_PORT;
         String listenAddress = null;
         String publicAddress = null;
-        float latitude = 0;
-        float longitude = 0;
         if(args.length>=1) port = Integer.parseInt(args[0].trim());
         if(args.length>=2) listenAddress = args[1].trim();
         if(args.length>=3) publicAddress = args[2].trim();
-        if(args.length>=5) {
-            latitude = Float.parseFloat(args[3]);
-            longitude = Float.parseFloat(args[4]);
-        }
-        if(args.length==4 || args.length>5) {
-            System.err.println("usage: "+MonitorServer.class.getName()+" [port [listen_address [public_address [latitude longitude]]]]");
-            System.err.println("\tport                    the server port - defaults to "+Monitor.DEFAULT_RMI_SERVER_PORT);
-            System.err.println("\tlisten_address          the local address the server will bind to - the private IP/hostname if behind NAT");
-            System.err.println("\tpublic_address          the public address that will reach the server - the external IP/hostname if behind NAT");
-            System.err.println("\tlatitude and longitude  the geographical position of the server");
+        if(args.length>3) {
+            System.err.println("usage: "+MonitorServer.class.getName()+" [port [listen_address [public_address]]]");
+            System.err.println("\tport            the server port - defaults to "+Monitor.DEFAULT_RMI_SERVER_PORT);
+            System.err.println("\tlisten_address  the local address the server will bind to - the private IP/hostname if behind NAT");
+            System.err.println("\tpublic_address  the public address that will reach the server - the external IP/hostname if behind NAT");
             System.exit(1);
             return;
         }
@@ -60,74 +51,61 @@ public class MonitorServer {
             System.setSecurityManager(new SecurityManager());
         }
         try {
-            // SSL
-            /* Will be set by scripts since each server has its own key
-            if(System.getProperty("javax.net.ssl.keyStorePassword")==null) {
-                System.setProperty(
-                    "javax.net.ssl.keyStorePassword",
-                    "changeit"
-                );
+            // Setup the RMI system properties
+            if(publicAddress!=null && publicAddress.length()>0) {
+                System.setProperty("java.rmi.server.hostname", publicAddress);
+            } else if(listenAddress!=null && listenAddress.length()>0) {
+                System.setProperty("java.rmi.server.hostname", listenAddress);
+            } else {
+                System.clearProperty("java.rmi.server.hostname");
             }
-            if(System.getProperty("javax.net.ssl.keyStore")==null) {
-                System.setProperty(
-                    "javax.net.ssl.keyStore",
-                    System.getProperty("user.home")+File.separatorChar+".keystore"
-                );
-            }
-             */
+            System.setProperty("java.rmi.server.randomIDs", "true");
+            System.setProperty("java.rmi.server.useCodebaseOnly", "true");
+            System.setProperty("java.rmi.server.disableHttp", "true");
 
-            // Start the RMI server
-            System.out.print("Starting MonitorServer: ");
-            boolean done = false;
-            while(!done) {
-                try {
-                    Monitor monitor = new MonitorImpl(
-                        new MonitoringPoint(
-                            publicAddress!=null ? publicAddress
-                            : listenAddress!=null ? listenAddress
-                            : InetAddress.getLocalHost().getCanonicalHostName(),
-                            latitude,
-                            longitude
-                        )
+            RMIClientSocketFactory csf;
+            RMIServerSocketFactory ssf;
+            if(
+                listenAddress!=null
+                && (
+                    listenAddress.equalsIgnoreCase("localhost")
+                    || listenAddress.equalsIgnoreCase("localhost.localdomain")
+                    || listenAddress.equals("127.0.0.1")
+                    || InetAddress.getByName(listenAddress).isLoopbackAddress()
+                )
+            ) {
+                // Non-SSL for anything loopback
+                csf = new RMIClientSocketFactoryTCP();
+                ssf = new RMIServerSocketFactoryTCP(listenAddress);
+            } else {
+                if(System.getProperty("javax.net.ssl.keyStorePassword")==null) {
+                    System.setProperty(
+                        "javax.net.ssl.keyStorePassword",
+                        "changeit"
                     );
-                    // TODO: threadlocale wrapper
-                    // TODO: trace wrapper
-                    // TODO: other wrappers like on aoserv-master (AOServ 2)
-                    monitorServer = RmiServerMonitor.getInstance(monitor, publicAddress, listenAddress, port);
-                    done = true;
-                    System.out.println("Done");
-                } catch(Exception err) {
-                    logger.log(Level.SEVERE, null, err);
-                    try {
-                        Thread.sleep(10000);
-                    } catch(InterruptedException err2) {
-                        logger.log(Level.WARNING, null, err2);
-                    }
                 }
-            }
+                if(System.getProperty("javax.net.ssl.keyStore")==null) {
+                    System.setProperty(
+                        "javax.net.ssl.keyStore",
+                        System.getProperty("user.home")+File.separatorChar+".keystore"
+                    );
+                }
 
-            // Start up the mobile server
-            System.out.print("Starting MobileServer: ");
-            done = false;
-            while(!done) {
-                try {
-                    mobileServer = new MobileServer(monitorServer, listenAddress);
-                    mobileServer.start();
-                    done = true;
-                    System.out.println("Done");
-                } catch(Exception err) {
-                    logger.log(Level.SEVERE, null, err);
-                    try {
-                        Thread.sleep(10000);
-                    } catch(InterruptedException err2) {
-                        logger.log(Level.WARNING, null, err2);
-                    }
+                // SSL for anything else
+                if(listenAddress!=null && listenAddress.length()>0) {
+                    csf = new RMIClientSocketFactorySSL();
+                    ssf = new RMIServerSocketFactorySSL(listenAddress);
+                } else {
+                    csf = new RMIClientSocketFactorySSL();
+                    ssf = new RMIServerSocketFactorySSL();
                 }
             }
+            Registry registry = LocateRegistry.createRegistry(port, csf, ssf); //LocateRegistry.getRegistry();
+            MonitorImpl monitor = new MonitorImpl(port, csf, ssf);
+            registry.rebind("com.aoindustries.noc.monitor.server.MonitorServer", monitor);
 
             // Auto-login with a top-level account to kick-off the monitoring if a username/password exist in the aoserv-client.properties file
             String rootUsername = AOServClientConfiguration.getUsername();
-            if(rootUsername!=null) rootUsername = rootUsername.trim();
             String rootPassword = AOServClientConfiguration.getPassword();
             if(
                 rootUsername!=null && (rootUsername=rootUsername.trim()).length()>0
@@ -136,7 +114,7 @@ public class MonitorServer {
                 int attemptsLeft = 120;
                 while(attemptsLeft>0) {
                     try {
-                        monitorServer.login(Locale.getDefault(), rootUsername, rootPassword);
+                        monitor.login(Locale.getDefault(), rootUsername, rootPassword);
                         break;
                     } catch(Exception err) {
                         logger.log(Level.SEVERE, null, err);
@@ -149,21 +127,14 @@ public class MonitorServer {
                     attemptsLeft--;
                 }
             }
-
-            // Avoid bug: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6597112
-            try {
-                Thread.sleep(300000); // Wait five minutes before losing reference to factory.
-            } catch(InterruptedException err) {
-                logger.log(Level.WARNING, null, err);
-            }
+            
+            // Start up the mobile server
+            new MobileServer(monitor, listenAddress).start();
         } catch(Exception err) {
             logger.log(Level.SEVERE, null, err);
         }
     }
 
-    /**
-     * Make no instances.
-     */
     private MonitorServer() {
     }
 }
