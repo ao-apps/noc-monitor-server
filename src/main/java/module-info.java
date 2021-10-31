c32d2b065ada48ad61bc84b63583ac6feb344ab7
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:56:43 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with noc-monitor-server.  If not, see <http://www.gnu.org/licenses/>.
+ * along with noc-monitor-server.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoindustries.noc.monitor.server {
 	exports com.aoindustries.noc.monitor.server;
