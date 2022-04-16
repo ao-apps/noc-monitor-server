2f0e09ebcda891f46805c6248a568759c762320e
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:23 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/noc/monitor/server/MobileServer.java
+++ src/main/java/com/aoindustries/noc/monitor/server/MobileServer.java
@@ -1,6 +1,6 @@
 /*
  * noc-monitor-server - Server for Network Operations Center Monitoring.
- * Copyright (C) 2008, 2009, 2017, 2018, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2008, 2009, 2017, 2018, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with noc-monitor-server.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.noc.monitor.server;
 
 import com.aoapps.lang.validation.ValidationException;
--- src/main/java/com/aoindustries/noc/monitor/server/MonitorServer.java
+++ src/main/java/com/aoindustries/noc/monitor/server/MonitorServer.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with noc-monitor-server.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.noc.monitor.server;
 
 import com.aoapps.hodgepodge.rmi.RMIClientSocketFactorySSL;
